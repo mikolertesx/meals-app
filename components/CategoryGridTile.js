6bd51131ae80b487839fd26ea1449fa0b940f551
Mikolertesx <mgangsal@outlook.com>
2020-11-21 12:34:57 -0600

    Fixing shadows in Categories

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -34,7 +34,11 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: "hidden",
+    overflow:
+      Platform.OS === "android" && Platform.version >= 21
+        ? "hidden"
+        : "visible",
+    elevation: 5,
   },
   container: {
     flex: 1,
@@ -43,7 +47,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.6,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: "flex-end",
     alignItems: "flex-end",
