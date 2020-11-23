e3271e76c205703ab1cc0b2bcefe8d97e39118e7
Mikolertesx <mgangsal@outlook.com>
2020-11-23 09:47:29 -0600

    Filters screen content

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { View, Text, StyleSheet, Switch, Platform } from "react-native";
+
 import HeaderButton from "../components/HeaderButton";
-import { View, Text, StyleSheet } from "react-native";
+import Colors from "../constants/Colors";
+
+const FilterSwitch = (props) => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        trackColor={{ true: Colors.primaryColor, false: "#ccc" }}
+        thumbColor={Platform.OS === "android" ? Colors.primaryColor : ""}
+        value={props.state}
+        onValueChange={props.onChange}
+      />
+    </View>
+  );
+};
 
 const FiltersScreen = (props) => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
   return (
     <View style={styles.screen}>
-      <Text>The Filters Screen!</Text>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <FilterSwitch
+        label="Gluten-free"
+        state={isGlutenFree}
+        onChange={(newValue) => setIsGlutenFree(newValue)}
+      />
+      <FilterSwitch
+        label="Lactose-free"
+        state={isLactoseFree}
+        onChange={(newValue) => setIsLactoseFree(newValue)}
+      />
+      <FilterSwitch
+        label="Vegan"
+        state={isVegan}
+        onChange={(newValue) => setIsVegan(newValue)}
+      />
+      <FilterSwitch
+        label="Vegetarian"
+        state={isVegetarian}
+        onChange={(newValue) => setIsVegetarian(newValue)}
+      />
     </View>
   );
 };
@@ -30,8 +71,19 @@ FiltersScreen.navigationOptions = (navData) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    margin: 20,
+    textAlign: "center",
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
   },
 });
 
