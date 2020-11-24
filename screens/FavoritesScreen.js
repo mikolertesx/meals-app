541666ee69cfe26c3823b34399fc0f161700765c
Mikolertesx <mgangsal@outlook.com>
2020-11-24 11:23:09 -0600

    Fallback text

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
+import DefaultText from "../components/DefaultText";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { useSelector } from "react-redux";
 
@@ -7,6 +9,14 @@ import MealList from "../components/MealList";
 
 const FavoritesScreen = (props) => {
   const favMeals = useSelector((state) => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No Favorite Meals Found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
 
   return <MealList navigation={props.navigation} listData={favMeals} />;
 };
@@ -27,5 +37,13 @@ FavoritesScreen.navigationOptions = (navData) => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default FavoritesScreen;
